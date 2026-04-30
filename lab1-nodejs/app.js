44fcde4ec43c1fb709b5eadeb4d2c1a955caa50f
Gul E Haram <guleharam7@gmail.com>
2026-04-30 23:08:23 +0500

    Exercise 1 done!

--- lab1-nodejs/app.js
+++ lab1-nodejs/app.js
@@ -1,13 +1,3 @@
-// This is my first Node.js program!
-// Lines starting with // are COMMENTS — Node.js ignores them
-// They are just notes for humans reading the code
-// console.log() prints a message to your terminal
 console.log('Hello! I am Gul E Haram!');
 console.log('Air University');
-// Variables store values
-let a = 10;
-let b = 20;
-// The + joins strings and numbers together
 console.log('I am ' + 19 + ' years old');
-// Showing the current date and time
-console.log('Right now it is: ' + new Date());
--- lab1-nodejs/server.js
+++ lab1-nodejs/server.js
@@ -1,21 +1,11 @@
-// &#39;require&#39; is how we load modules in Node.js
-// &#39;http&#39; is a built-in module — it comes with Node, no install needed
 const http = require('http');
-// createServer() creates a web server
-// It takes a function that runs EVERY TIME someone visits
-// req = the incoming request from the user (what they asked for)
-// res = the response we send back to the user
-const msg = 'Welcome to my server! Built by Gul E Haram';
+const msg = 'My first server!';
 const server = http.createServer(function(req, res) {
-// Tell the browser what type of content we are sending
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-// Send a message back to the browser
-res.end(msg);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Welcome to my server! Built by Gul E Haram\n')
+    res.end(msg);
 });
-// Tell the server to listen on port 3000
-// Port 3000 is like a door number on your computer
-// localhost means 'this computer'
 server.listen(3000, function() {
-console.log('Server is running!');
-console.log('Open your browser and go to: http://localhost:3000');
+    console.log('Server is running!');
+    console.log('Open your browser and go to: http://localhost:3000');
 });
